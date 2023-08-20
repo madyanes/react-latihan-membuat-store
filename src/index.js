c9bfaf405189e33027e8929c3723a20f870857ee
madyanes <madyanes.dev@gmail.com>
2023-08-21 02:21:28 +0700

    Creating store Membuat store, yang mana merupakan sebuah objek

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function createStore() {
+  /**
+   * Store memiliki 4 hal:
+   * 1. State
+   * 2. Mendapatkan state
+   * 3. Men-subscribe perubahan state
+   * 4. Memperbarui state
+   */
+
+  // mengembalikan objek (store)
+  return {}
+}
