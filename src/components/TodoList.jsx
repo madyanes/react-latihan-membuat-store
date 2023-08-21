f17a8712c514a8304b275ed073ec8352974e5fc3
madyanes <madyanes.dev@gmail.com>
2023-08-21 14:54:46 +0700

    Dispatching todo actions

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodoActionCreator, deleteTodoActionCreator, toggleTodoActionCreator } from '../states/todos/action';
 import TodoInput from './TodoInput';
 import TodoItem from './TodoItem';
 
 function TodoList() {
   const todos = useSelector((states) => states.todos); // TODO: Get todos from store;
+  const dispatch = useDispatch();
 
   function onAddTodo(text) {
-    // TODO: dispatch action ADD_TODO
+    const id = `todo-${+new Date()}`;
+    dispatch(
+      addTodoActionCreator({ id, text })
+    );
   }
 
   function onToggleTodo(id) {
-    // TODO: dispatch action TOGGLE_TODO
+    dispatch(
+      toggleTodoActionCreator(id)
+    );
   }
 
   function onDeleteTodo(id) {
-    // TODO: dispatch action DELETE_TODO
+    dispatch(
+      deleteTodoActionCreator(id)
+    );
   }
 
   return (
