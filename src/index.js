ef4533a2cbce82b39713ca4fcca52418b46c6503
madyanes <madyanes.dev@gmail.com>
2023-08-21 09:17:44 +0700

    Create action to delete todo

--- src/index.js
+++ src/index.js
@@ -48,12 +48,23 @@ function addTodoActionCreator({ id, text }) {
   }
 }
 
+function deleteTodoActionCreator(id) {
+  return {
+    type: 'DELETE_TODO',
+    payload: { id }
+  }
+}
+
 // fungsi reducer sebagai perantara STATE dan ACTION, bertugas memperbarui nilai state
 // membutuhkan parameter state saat ini dan parameter action untuk memanipulasi state tsb
 // mengembalikan state dengan nilai yang telah diperbarui
 function todosReducer(todos = [], action = {}) {
   if (action.type === 'ADD_TODO') {
     return [...todos, action.payload]
+  }
+
+  if (action.type === 'DELETE_TODO') {
+    return todos.filter((todo) => todo.id !== action.payload.id)
   }
 
   return todos
@@ -78,3 +89,6 @@ store.dispatch(
 store.dispatch(
   addTodoActionCreator({ id: 3, text: 'Learn JavaScript' })
 )
+
+// menghapus todo dengan id 3
+store.dispatch(deleteTodoActionCreator(3))
