6fceeee517ab58aebd77b250846a99ded77a72dd
madyanes <madyanes.dev@gmail.com>
2023-08-21 09:23:14 +0700

    Isolating createStore() so it can be used anywhere

--- src/index.js
+++ src/index.js
@@ -1,40 +1,4 @@
-function createStore() {
-  /**
-   * Store memiliki 4 hal:
-   * 1. State
-   * 2. Mendapatkan state
-   * 3. Men-subscribe perubahan state
-   * 4. Memperbarui state
-   */
-
-  let state
-  let listeners = []
-
-  const getState = () => state
-
-  const subscribe = (listener) => {
-    listeners.push(listener)
-
-    return () => {
-      listeners = listeners.filter((listenerItem) => listenerItem !== listener)
-    }
-  }
-
-  // akan men-trigger `store` untuk mengubah state sesuai dengan action yang diberikan pada argumen
-  // selain itu, dispatch juga akan memanggil seluruh `listeners` dalam store dengan tujuan untuk memberitahu bahwa state dalam store berubah
-  // jadi, nantinya UI bisa secara reaktif me-render setiap kali ada perubahan state
-  const dispatch = (action) => {
-    state = todosReducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
-
-  // mengembalikan objek (store)
-  return {
-    getState,
-    subscribe,
-    dispatch
-  }
-}
+import { createStore } from './redux'
 
 // fungsi action creator merupakan fungsi yang menghindari kesalahan-kesalahan dalam membuat objek action seperti tipo atau inkonsisten dalam menuliskan struktur propertinya
 function addTodoActionCreator({ id, text }) {
@@ -71,7 +35,7 @@ function todosReducer(todos = [], action = {}) {
 }
 
 // consume
-const store = createStore()
+const store = createStore(todosReducer)
 
 // subscribe to state changes
 store.subscribe(() => {
